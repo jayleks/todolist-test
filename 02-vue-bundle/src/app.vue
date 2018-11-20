74d9d6ad064a63d1b1c68aa5f8b82fa30491136b
jayleks <jayleks@ya.ru>
2018-11-20 12:34:16 +0300

    append vue version with bundle

---
+++ 02-vue-bundle/src/app.vue
@@ -0,0 +1,47 @@
+<template src="./templ/app.html"></template>
+ 
+<script>
+
+import helper from './helper'
+
+export default {
+
+    data() {
+        return {
+            items: [],
+            newItem: ''
+        }
+    },
+
+    computed: {
+        total: function () {
+            return this.items.length;
+        },
+
+        done: function () {
+            return this.items.filter(function (item) {
+                return item.isDone;
+            }).length;
+        }
+    },
+
+    methods: {
+        addItem: function () {
+            var value = this.newItem && this.newItem.trim()
+            if (!value) return;
+
+            this.items.push({
+                id: helper.getGuid(),
+                name: value,
+                isDone: false
+            })
+            this.newItem = ''
+        },
+
+        removeItem: function (item) {
+            this.items.splice(this.items.indexOf(item), 1);
+        }
+    }
+}
+
+</script>
---
+++ 02-vue-bundle/src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './app.vue'
+
+new Vue({
+  el: '#todo-container',
+  render: h => h(App)
+})
